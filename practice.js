5432759fa72cc2e3cfa231d31902ffb6d4306249
Zhijie Fang <847749593@qq.com>
2023-11-17 09:19:26 +0800

    test

--- practice.js
+++ practice.js
@@ -2,12 +2,13 @@ const express = require('./index')
 
 const app = express()
 
-let n = 0
-
-app.all('/*', function (req, res, next) {
-  if (n++) return console.error(new Error('DELETE called several times'))
+app.get('/users', function (req, res) {})
+app.all('/users', function (req, res, next) {
+  res.setHeader('x-hit', 1)
+  res.send('ss')
   next()
 })
+
 app.listen(3000, () => {
   console.log('the server is running at port 3000')
 })
---
+++ test/app.head.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const express = require('../')
+const request = require('supertest')
+const assert = require('assert')
+
+describe('HEAD', function () {
+  it('should default to GET', function (done) {
+    const app = express()
+
+    app.get('/tobi', function (req, res) {
+      res.send('tobi')
+    })
+
+    request(app).head('/tobi').expect(200, done)
+  })
+
+  it('should output the same headers as GET requests', function (done) {
+    const app = express()
+
+    app.get('/tobi', function (req, res) {
+      res.send('tobi')
+    })
+
+    request(app)
+      .head('/tobi')
+      .expect(200, function (err, res) {
+        if (err) return done(err)
+        const headers = res.headers
+
+        request(app)
+          .get('/tobi')
+          .expect(200, function (err, res) {
+            if (err) return done(err)
+            delete headers.date
+            delete res.headers.date
+            assert.deepEqual(res.headers, headers)
+            done()
+          })
+      })
+  })
+})
+
+describe('app.head()', function () {
+  it('should override', function (done) {
+    const app = express()
+
+    app.head('/tobi', function (req, res) {
+      res.header('x-method', 'head')
+      res.end()
+    })
+
+    app.get('/tobi', function (req, res) {
+      res.header('x-method', 'get')
+      res.send('tobi')
+    })
+
+    request(app).head('/tobi').expect('x-method', 'head').expect(200, done)
+  })
+})
