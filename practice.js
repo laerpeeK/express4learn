cc4255798f16ecabc9fcef770d85090537cce86a
Zhijie Fang <847749593@qq.com>
2023-11-26 15:19:34 +0800

    serve-static

--- practice.js
+++ practice.js
@@ -1,1 +1,11 @@
-const http = require('node:http')
+const express = require('./index.js')
+const app = express()
+const path = require('path')
+
+app.use(
+  express.static(path.join(__dirname, './test/fixtures'), { reirect: true })
+)
+
+app.listen(3000, () => {
+  console.log('server running...')
+})
