22aecf50a81b2a9a544d5fe1a3e856e33770a597
Zhijie Fang <847749593@qq.com>
2023-11-26 14:06:46 +0800

    doc

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# mini-express4
+# learn-express4
+## 说明
+该仓库仅作个人学习express源码使用，对其中的代码(包括主体代码跟单元测试代码进行了改动)。如果您要使用express开发，请访问原框架链接[express](https://github.com/expressjs/express)
 
-## 阅读Express源码的建议
-
-## 参考文章
-[多图理解 Express](https://juejin.cn/post/6904561783437000718#comment)
--- practice.js
+++ practice.js
@@ -1,16 +1,1 @@
-
-const express = require('./')
-const path = require('path')
-const fixtures = path.join(__dirname, './test/fixtures')
-const app = express()
-const root = fixtures
-
-app.use(express.static(root))
-
-app.use(function (req, res, next) {
-  res.sendStatus(404)
-})
-
-app.listen(3000, function () {
-  console.log('running at port 3000')
-})
+const http = require('node:http')
